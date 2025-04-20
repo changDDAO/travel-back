241c603876fe8dd180bbe047d5cd18d236f75e98
legoChoi <chlx159@gmail.com>
2025-04-20 22:24:04 +0900

    :sparkles: feat: add security, jwt base

---
+++ src/main/java/com/project/travel/common/security/handler/SecurityAuthenticationEntryPoint.java
@@ -0,0 +1,37 @@
+package com.project.travel.common.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SecurityAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException
+    ) throws IOException {
+        log.warn("Security Authentication error: need to login");
+
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(status.value());
+//        response.getWriter().write(objectMapper.writeValueAsString(
+//                new ErrorResponse(status, authException.getMessage())));
+    }
+}

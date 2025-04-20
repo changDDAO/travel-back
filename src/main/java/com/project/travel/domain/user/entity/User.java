45cc15baabbdbaa82a6f1517d84aa12fdc7da64d
legoChoi <chlx159@gmail.com>
2025-04-20 22:19:49 +0900

    :sparkles: feat: add additional columns to user entity

--- src/main/java/com/project/travel/domain/user/entity/User.java
+++ src/main/java/com/project/travel/domain/user/entity/User.java
@@ -9,10 +9,35 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 
 @Entity
-@Table(name = "users")
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "idx_u_email", columnNames = { "email" }),
+                @UniqueConstraint(name = "idx_u_nickname", columnNames = { "nickname" })
+        }
+)
 public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
 }
